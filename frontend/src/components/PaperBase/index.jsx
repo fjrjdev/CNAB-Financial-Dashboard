ad67867ffa900e5b48d9371d773d74528b1c5742
fjrjdev <thedromemafia@gmail.com>
2022-11-25 17:25:39 -0300

    feat: added github link

--- frontend/src/components/PaperBase/index.jsx
+++ frontend/src/components/PaperBase/index.jsx
@@ -13,8 +13,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/fjrjdev">
+        Fabio Junior
       </Link>{" "}
       {new Date().getFullYear()}.
     </Typography>
